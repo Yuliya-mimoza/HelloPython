853acc8ecdf340a214b456cf3ad1c5eb20ba36c1
Yuliya <m.i.m.o.z.a@mail.ru>
2022-12-22 09:03:43 +0400

    Программа, которая принимает на вход число N и выдаёт последовательность из N членов

--- Seminars/Seminar2/Task2_1.py
+++ Seminars/Seminar2/Task2_1.py
@@ -3,4 +3,7 @@
 # - Для N = 5: 1, -3, 9, -27, 81
 n = int(input("Введите число: "))
 for i in range(n):
-    print ((-3)**i)
+    if i != n - 1:
+        print((-3)**i, end=', ')
+else:
+    print((-3)**i)
