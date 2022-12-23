06d277bf39616229f77cd7e3f3b17d20e204926a
Yuliya <m.i.m.o.z.a@mail.ru>
2022-12-23 15:51:00 +0400

    Реализуйте алгоритм перемешивания списка

---
+++ HomeWork/HomeWork2/HomeWork2_4.py
@@ -0,0 +1,6 @@
+# Реализуйте алгоритм перемешивания списка.
+some_list=[1,2,3,4,5,6,7,8,9,0]
+import random
+print("Текущий список: ",some_list)
+random.shuffle(some_list)
+print("Список после перемешивания: ",some_list)
